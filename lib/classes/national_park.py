c88694699ae8a1707940c9b3aeee19854af2f5f6
Sarah Jones <sarahrjones8@gmail.com>
2023-05-08 18:36:29 -0400

    complete lab

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -4,17 +4,45 @@ class NationalPark:
         self.name = name
         self._trips = []
         self._visitors = []
+        self.visitors_count = {}
+        
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if isinstance(name, str) and not hasattr(self, 'name'):
+            self._name = name
+        else:
+            raise Exception("Name must not exist and must be a string")
+        
+    name = property(get_name, set_name)
         
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
+        if new_trip and type(new_trip) == Trip:
+            self._trips.append(new_trip)
+            
+        return self._trips
+        
     
     def visitors(self, new_visitor=None):
         from classes.visitor import Visitor
-        pass
+        
+        if new_visitor and type(new_visitor) == Visitor and new_visitor not in self._visitors:
+            self._visitors.append(new_visitor)
+            
+        return self._visitors
+        
     
     def total_visits(self):
-        pass
+        return len(self._trips)
     
     def best_visitor(self):
-        pass
+        for trip in self._trips:
+            # looping over the trips
+            if trip.visitor in self.visitors_count:
+                self.visitors_count[trip.visitor] += 1
+            else: 
+                self.visitors_count[trip.visitor] = 1
+        return max(self.visitors_count, key=self.visitors_count.get)
+            
--- lib/classes/visitor.py
+++ lib/classes/visitor.py
@@ -2,11 +2,40 @@ class Visitor:
 
     def __init__(self, name):
         self.name = name
+        self._trips = []
+        self._national_parks = []
+        
+    def get_name(self): 
+        return self._name
+    
+    def set_name(self, name):
+        # must be a str
+        # must be between 1 to 15 characters
+        # cannot be changed once it has been initialized
+        
+        if type(name) == str and len(name) >= 1 and len(name) <= 15 and not hasattr(self, 'name'):
+            self._name = name
+            
+        else:
+            raise Exception("Name is not a string or not between 1 to 15 characters or name already exists")
+        
+    name = property(get_name, set_name)
         
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
+        if new_trip and type(new_trip) == Trip:
+            self._trips.append(new_trip)
+            
+        return self._trips
     
     def national_parks(self, new_national_park=None):
         from classes.national_park import NationalPark
-        pass
+        if new_national_park and type(new_national_park) == NationalPark and new_national_park not in self._national_parks:
+            self._national_parks.append(new_national_park)
+            
+        return self._national_parks
+        
+
+
+
+    
